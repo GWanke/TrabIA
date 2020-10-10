90532c5bbb9642889d50e0be9d67e2008e8a50a0
GWanke <ra91671@uem.br>
2020-10-10 03:46:08 -0300

    AG fixacom hash.(demora cerca de 300 iteracoes, termina em menos de 10s sem print.)

--- T1.py
+++ T1.py
@@ -1,6 +1,7 @@
 import classes
 import random
 from random import randint
+import itertools
 #def Mutacao():
 #def Genetico():
 #def Agente():
@@ -13,8 +14,9 @@ def readData(linha):
 	e.min = int(''.join(map(str,linha[83:95])))
 	e.med = int(''.join(map(str,linha[96:108])))
 	return e
+
 def readInput():
-	with open("teste.TXT", "r") as k:
+	with open("COTAHIST_A2014.TXT", "r") as k, open("COTAHIST_A2015.TXT", "r") as f:
 		for line in k:
 			listaux = list(line) 
 			if (line[27:39] == "METAL LEVE  "):          #1
@@ -48,11 +50,45 @@ def readInput():
 				e=readData(listaux)
 				classes.Empresa().EmpresaJ.append(e)
 			listaux *= 0
+		for line in f:
+			listaux = list(line) 
+			if (line[27:39] == "METAL LEVE  "):          #1
+				e=readData(listaux)
+				classes.Empresa().EmpresaA.append(e)     
+			elif (line[27:39] == "WEG         "):        #2
+				e=readData(listaux)		 
+				classes.Empresa().EmpresaB.append(e)     
+			elif (line[27:39] == "NATURA      "):        #3
+				e=readData(listaux)
+				classes.Empresa().EmpresaC.append(e)
+			elif (line[27:39] == "SABESP      "):        #4
+				e=readData(listaux)
+				classes.Empresa().EmpresaD.append(e)
+			elif (line[27:39] == "SID NACIONAL"):  
+				e=readData(listaux)
+				classes.Empresa().EmpresaE.append(e)
+			elif (line[27:39] == "PFIZER      "):        #6
+				e=readData(listaux)
+				classes.Empresa().EmpresaF.append(e)
+			elif (line[27:39] == "LOJAS RENNER"):        #7
+				e=readData(listaux)
+				classes.Empresa().EmpresaG.append(e)
+			elif (line[27:39] == "CIELO       "):        #8
+				e=readData(listaux)
+				classes.Empresa().EmpresaH.append(e)
+			elif (line[27:39] == "TELEF BRASIL"):        #9
+				e=readData(listaux)
+				classes.Empresa().EmpresaI.append(e)
+			elif (line[27:39] == "GRENDENE    "):        #10
+				e=readData(listaux)
+				classes.Empresa().EmpresaJ.append(e)
+			listaux *= 0			
 	k.close()
+	f.close()
 def geraPopulacaoInicio():
 	aux = []
 	carteira = []
-	popMax = 15
+	popMax = 20
 	for pop in range (popMax):
 		rngA = randint(0,100) 
 		rngB = randint(0,100)
@@ -76,83 +112,86 @@ def geraPopulacaoInicio():
 		compraI = rngI * fator
 		compraJ = rngJ * fator
 		somatorio = compraA + compraB + compraC + compraD + compraE + compraF + compraG + compraH + compraI + compraJ
-		aux = [round(compraA,2),round(compraB,2),round(compraC,2),round(compraD,2),round(compraE,2),round(compraF,2),round(compraG,2),round(compraH,2),round(compraI,2),round(compraJ,2)]
+		aux = [(compraA),(compraB),(compraC),(compraD),(compraE),(compraF),(compraG),(compraH),(compraI),(compraJ),]
 		carteira.append(aux)
 	return carteira
-def CalculoMedia(opc):
-	soma = 0
-	med = 0
-	if opc ==1 :
-		for x in range(len(classes.Empresa().EmpresaA)):
-			soma = soma + classes.Empresa().EmpresaA[x].med
-		med = soma / len(classes.Empresa().EmpresaA)
-	elif opc ==2 :
-		for x in range(len(classes.Empresa().EmpresaB)):
-			soma = soma + classes.Empresa().EmpresaB[x].med
-		med = soma / len(classes.Empresa().EmpresaB)
-	elif opc ==3 :
-		for x in range(len(classes.Empresa().EmpresaC)):
-			soma = soma + classes.Empresa().EmpresaC[x].med
-		med = soma / len(classes.Empresa().EmpresaC)
-	elif opc ==4 :
-		for x in range(len(classes.Empresa().EmpresaD)):
-			soma = soma + classes.Empresa().EmpresaD[x].med
-		med = soma / len(classes.Empresa().EmpresaD)
-	elif opc ==5 :
-		for x in range(len(classes.Empresa().EmpresaE)):
-			soma = soma + classes.Empresa().EmpresaE[x].med
-		med = soma / len(classes.Empresa().EmpresaE)
-	elif opc ==6 :
-		for x in range(len(classes.Empresa().EmpresaF)):
-			soma = soma + classes.Empresa().EmpresaF[x].med
-		med = soma / len(classes.Empresa().EmpresaF)
-	elif opc ==7 :
-		for x in range(len(classes.Empresa().EmpresaG)):
-			soma = soma + classes.Empresa().EmpresaG[x].med
-		med = soma / len(classes.Empresa().EmpresaG)
-	elif opc ==8 :
-		for x in range(len(classes.Empresa().EmpresaH)):
-			soma = soma + classes.Empresa().EmpresaH[x].med
-		med = soma / len(classes.Empresa().EmpresaH)
-	elif opc ==9 :
-		for x in range(len(classes.Empresa().EmpresaI)):
-			soma = soma + classes.Empresa().EmpresaI[x].med
-		med = soma / len(classes.Empresa().EmpresaI)
-	elif opc ==10 :
-		for x in range(len(classes.Empresa().EmpresaJ)):
-			soma = soma + classes.Empresa().EmpresaJ[x].med
-		med = soma / len(classes.Empresa().EmpresaJ)
-	return med
+def hash():
+	dici = {}
+	soma=0
+	for x in range(len(classes.Empresa().EmpresaA)):
+		soma = soma + classes.Empresa().EmpresaA[x].med
+	dici[1]  = soma / len(classes.Empresa().EmpresaA)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaB)):
+		soma = soma + classes.Empresa().EmpresaB[x].med
+	dici[2]  = soma / len(classes.Empresa().EmpresaB)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaC)):
+		soma = soma + classes.Empresa().EmpresaC[x].med
+	dici[3]  = soma / len(classes.Empresa().EmpresaC)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaD)):
+		soma = soma + classes.Empresa().EmpresaD[x].med
+	dici[4]  = soma / len(classes.Empresa().EmpresaD)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaE)):
+		soma = soma + classes.Empresa().EmpresaE[x].med
+	dici[5]  = soma / len(classes.Empresa().EmpresaE)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaF)):
+		soma = soma + classes.Empresa().EmpresaF[x].med
+	dici[6]  = soma / len(classes.Empresa().EmpresaF)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaG)):
+		soma = soma + classes.Empresa().EmpresaG[x].med
+	dici[7]  = soma / len(classes.Empresa().EmpresaG)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaH)):
+		soma = soma + classes.Empresa().EmpresaH[x].med
+	dici[8]  = soma / len(classes.Empresa().EmpresaH)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaI)):
+		soma = soma + classes.Empresa().EmpresaI[x].med
+	dici[9]  = soma / len(classes.Empresa().EmpresaI)
+	soma =0
+	for x in range(len(classes.Empresa().EmpresaJ)):
+		soma = soma + classes.Empresa().EmpresaJ[x].med
+	dici[10]  = soma / len(classes.Empresa().EmpresaJ)
+	soma =0
+	return dici
+def CalculoMedia(opc,dici):
+	return dici[opc]	
 
-def CalculoFitness(carteira):
+
+def CalculoFitness(carteira,dici):
 	listasorted = []
-	v = sorted(carteira,key=lambda k:CalculoFitness2(k))[::-1]
+	v = sorted(carteira,key=lambda k:CalculoFitness2(k,dici))[::-1]
 	for item in v:
 		indexv = carteira.index(item)
 		listasorted.append(indexv)
 	return listasorted
-def CalculoFitness2(filho):
+def CalculoFitness2(filho,dici):
 	aux = []
 	ganho = []
 	for valor in filho:
-		ganhoA = valor * CalculoMedia(1)
-		ganhoB = valor * CalculoMedia(2)
-		ganhoC = valor * CalculoMedia(3)
-		ganhoD = valor * CalculoMedia(4)
-		ganhoE = valor * CalculoMedia(5)
-		ganhoF = valor * CalculoMedia(6)
-		ganhoG = valor * CalculoMedia(7)
-		ganhoH = valor * CalculoMedia(8)
-		ganhoI = valor * CalculoMedia(9)
-		ganhoJ = valor * CalculoMedia(10)
+		ganhoA = valor * CalculoMedia(1,dici)
+		ganhoB = valor * CalculoMedia(2,dici)
+		ganhoC = valor * CalculoMedia(3,dici)
+		ganhoD = valor * CalculoMedia(4,dici)
+		ganhoE = valor * CalculoMedia(5,dici)
+		ganhoF = valor * CalculoMedia(6,dici)
+		ganhoG = valor * CalculoMedia(7,dici)
+		ganhoH = valor * CalculoMedia(8,dici)
+		ganhoI = valor * CalculoMedia(9,dici)
+		ganhoJ = valor * CalculoMedia(10,dici)
 		somatorioGanho = ganhoA + ganhoB + ganhoC + ganhoD + ganhoE + ganhoF + ganhoG + ganhoH + ganhoI + ganhoJ #somatorio do lucro medio,baseado nos dados.
 	z = somatorioGanho / len(filho)
 	somatorioGanho =0
 	return z
 		
-def ComparaPop(index,carteira,filhos):
+def ComparaPop(index,carteira,filhos,dici):
 	for filho in filhos: 
-		if CalculoFitness2(carteira[index[len(index)-1]]) <= CalculoFitness2(filho) and filho not in carteira:
+		if CalculoFitness2(carteira[index[len(index)-1]],dici) <= CalculoFitness2(filho,dici) and filho not in carteira:
 			carteira.pop(index[len(index)-1])
 			carteira.append(filho)
 	return carteira 
@@ -192,6 +231,7 @@ def Crossover(selecionados,carteira):
 		ac2=0
 		fator1=0
 		fator2=0
+		soma=0
 		pai1 = carteira[selecionados[0]]
 		pai2 = carteira[selecionados[1]]
 		rng = randint(0,9)
@@ -202,15 +242,15 @@ def Crossover(selecionados,carteira):
 			else:
 				ac1 += pai2[x]
 				ac2 += pai1[x]
-		fator1 = round(1/ac1,2)
-		fator2 = round(1/ac2,2)
+		fator1 = round(1.0/ac1,4)
+		fator2 = round(1.0/ac2,4)
 		for i in range (0,10):
-			if x<rng:
+			if i<rng:
 				filho1.append(pai1[i]*fator1)
 				filho2.append(pai2[i]*fator2)
 			else:
 				filho1.append(pai2[i]*fator1)
-				filho2.append(pai2[i]*fator2)
+				filho2.append(pai1[i]*fator2)
 		filhos.append(filho1)
 		filhos.append(filho2)
 		selecionados.pop(0)
@@ -221,12 +261,12 @@ def Mutacao(filhos):
 	#Mutara o individuo em dois genes, que deverao obedecer a regra da soma equivalente a um do individuo.Em outras palavras, serao dois novos valores com a mesma soma.
 	probabilidade=randint(0,100)
 	tamfilhos=len(filhos)-1
-	if probabilidade<15:
+	if probabilidade<0:
 		index = randint(0,tamfilhos)
 		rng1,rng2 = random.sample(range(0,10),2)
 		somapeso = filhos[index][rng1] + filhos[index][rng2]
-		filhos[index][rng1] = round(random.uniform(0,filhos[index][rng1]),2)
-		filhos[index][rng2] = round(somapeso - filhos[index][rng1],2)
+		filhos[index][rng1] = random.uniform(0,filhos[index][rng1])
+		filhos[index][rng2] = somapeso - filhos[index][rng1]
 	return filhos
 def genetico():
 	carteira = []
@@ -234,44 +274,33 @@ def genetico():
 	numRep=0
 	soma=0
 	med=0
+	valor=0
 	readInput()
+	dici=hash()
 	carteira = geraPopulacaoInicio()
-	vantigo =  CalculoFitness2(carteira[0])
-	for i in carteira:
-		soma = soma + CalculoFitness2(i)
-	med = soma/len(carteira)
-	print "media antiga : " ,med
-	while(respostaIgualRepetida<=15 and numRep<100):
+	while(respostaIgualRepetida<50 and numRep<10000):
 		indices = []
 		selecionados = []
 		filhos = []
 		filhosmuta = []
-		respostaAntiga = CalculoFitness(carteira)
-		#print "RA",(respostaAntiga)
-		print CalculoFitness2(carteira[respostaAntiga[0]]),respostaAntiga
+		respostaAntiga = CalculoFitness(carteira,dici) 
 		selecionados = Selecao(respostaAntiga)
-		#print "selecionados", selecionados
 		filhos=Crossover(selecionados,carteira)
-		#print "filhos", filhos
 		filhosmuta=Mutacao(filhos)
-		#print "filhosmuta", filhosmuta
-		carteira=ComparaPop(respostaAntiga,carteira,filhosmuta)
-		#print "carteira", carteira
-		novaResposta = CalculoFitness(carteira)
-		#print "nR", novaResposta
-		print CalculoFitness2(carteira[novaResposta[0]]),novaResposta
-		if (respostaAntiga==novaResposta):
+		carteira=ComparaPop(respostaAntiga,carteira,filhosmuta,dici)
+		novaResposta = CalculoFitness(carteira,dici)
+		if (CalculoFitness2(carteira[respostaAntiga[0]],dici)==CalculoFitness2(carteira[novaResposta[0]],dici)):
 			respostaIgualRepetida +=1
 		else:
-			respostaIgualRepetida==0
+			respostaIgualRepetida=0
 		numRep += 1
+	return carteira[novaResposta[0]],CalculoFitness2(carteira[novaResposta[0]],dici)
 	soma=0
 	med=0
-	for it in carteira:
-		soma = soma + CalculoFitness2(it)
-	med = soma/len(carteira)
-	print "media nova = " , med
+
 def main():
-	genetico()
+	carteira = []
+	sol = 0
+	carteira,sol = genetico()
 if __name__ == '__main__':
 	main()
